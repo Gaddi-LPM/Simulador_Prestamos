f6232260801afa301dde248d32e2d7cbf639f230
Matias Gaddi <gaddi.matias@gmail.com>
2022-08-11 03:53:55 -0300

    fech

--- simulador.js
+++ simulador.js
@@ -1,18 +1,19 @@
 
+let contenedor = document.getElementById("container");
+let contenedor_dos = document.getElementById("container_dos");
+let btn_confirmar = document.getElementById("confirmar");
 let btn = document.getElementById("boton");
 let borrar = document.getElementById("borrar");
 let finalizado = document.getElementById("confirmar");
 let Consultadorr = document.getElementById("Consultador");
 let btn_solicitar = document.getElementById("btn_solicitar");
+let = dato_clima = document.getElementById("dato_clima");
 
-btn.addEventListener("click", carga_datos);
-function carga_datos(){
+btn.addEventListener("click", () => {
 
     let banco = document.getElementById("banco").value.toUpperCase();
     let monto = document.getElementById("monto").value;
     let plazo = document.getElementById("plazo").value;
-    let contenedor = document.getElementById("container");
-    let btn_confirmar = document.getElementById("confirmar");
 
     let information = {"Entidad": banco , "Cantidad": monto , "Cuotas": plazo };
     Consulta_info.push(information);
@@ -35,8 +36,9 @@ function carga_datos(){
                              <p> El Interes de la cuota solicitada es <strong> ${interes.toFixed(2)} </strong> </p>
                              <p> El (CFT) a pagar mensualmente es <strong> ${cuota_total.toFixed(2)} </strong> </p>`;
                              btn_confirmar.hidden = false; 
-                             btn_confirmar.style.display = "flex";                        
-}
+                             btn_confirmar.style.display = "flex"; 
+
+});
 
 let Consulta_info = [];
 
@@ -74,10 +76,15 @@ btn_solicitar.addEventListener("click", () => {
     let btn_nombre = document.getElementById("btn_nombre").value;
     let btn_dni = document.getElementById("btn_dni").value;
     let contenedor_dos = document.getElementById("container_dos");
-    btn_nombre != "" && btn_dni != "" ? dar_alta () : contenedor_dos.innerHTML = `<p> Ingrese Nombre y Apellido </p>
-                                                                                  <p> Ingrese Nro de Dni`;
+    btn_nombre != "" && btn_dni != "" ? dar_alta () : contenedor_dos.innerHTML = `<p> Ingrese Datos Solicitados </p>`;
 
 })
+
+let btn_nombre = document.getElementById("btn_nombre");
+btn_nombre.addEventListener("focus", (e) => {
+
+    e.target != "" ? contenedor_dos.style.display = "none" : contenedor_dos.style.display = "block";
+});
 
 let dar_alta = () =>{
     Swal.fire({
@@ -88,11 +95,29 @@ let dar_alta = () =>{
         imageWidth: 400,
         imageHeight: 200,
         imageAlt: 'Custom image',
+        button: 'simulador.html'
       })
+
+      setTimeout( ()=> window.location.href = "simulador.html"  ,4000);
+    
 }
 
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&lang=sp&units=metric&appid=e1e3c5b80aea982acc9e895d320a90b6")
 
-
+    .then(response => response.json())
+    .then(data => {
+        let icon_code = data.weather[0].icon;
+        let url_icon = `http://openweathermap.org/img/wn/${icon_code}.png`;
+    
+        dato_clima.innerHTML = `<p> Temp ${data.main.temp} °C </p>
+                                <p> Viento ${data.wind.speed} m/S </p> 
+                                <p> Rafagas ${data.wind.gust} </p>
+                                <p>  ${data.weather[0].description} </p>
+                                <img src = ${url_icon}>
+                                <p> <strong>${data.name} </strong> </p>`;
+                                console.log(data);
+                                
+    })
 
 
 
